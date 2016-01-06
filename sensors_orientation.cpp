5333bf2e9c8c0e9d5f36d23dda18f5b3d1f2bc80
Aaron <acmiller273@gmail.com>
2016-01-06 00:42:22 -0500

    Implemented IMU sensor readings

--- sensors_orientation.cpp
+++ sensors_orientation.cpp
@@ -1,5 +1,148 @@
 #include "sensors_orientation.h"
+#include "conf.h"
 
-void orientation_update() {
-    
+#include <Arduino.h>
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+Orientation::Orientation() {
+  Wire.begin();
+  Wire.setClock(400000);
+
+  mpu_.initialize();
+  Serial2.println(mpu_.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  uint8_t dev_status = mpu_.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu_.setXGyroOffset(0);
+  mpu_.setYGyroOffset(-60);
+  mpu_.setZGyroOffset(-24);
+  mpu_.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (dev_status == 0) {
+    // turn on the DMP, now that it's ready
+    Serial2.println(F("Enabling DMP..."));
+    mpu_.setDMPEnabled(true);
+
+    // enable interrupt detection
+    Serial2.println(F("Enabling MPU6050 interrupt detection..."));
+    attachInterrupt(digitalPinToInterrupt(IMU_INTERRUPT_PIN), interruptHandler, RISING);
+
+    // set our DMP Ready flag so the main update() function knows it's okay to use it
+    Serial2.println(F("DMP ready! Waiting for first interrupt..."));
+    dmp_ready_ = true;
+
+    // get expected DMP packet size for later comparison
+    packet_size_ = mpu_.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial2.print(F("DMP Initialization failed (code "));
+    Serial2.print(dev_status);
+    Serial2.println(F(")"));
+  }
 }
+
+void Orientation::interruptHandler() {
+  mpu_interrupt_ = true;
+}
+
+Orientation* Orientation::getInstance() {
+  if (instance_ == NULL) {
+    mpu_interrupt_ = false;
+    instance_ = new Orientation();
+  }
+  return instance_;
+}
+
+void Orientation::update() {
+  if (!dmp_ready_) return;
+
+  if (!mpu_interrupt_ && fifo_count_ < packet_size_) {
+    return;
+  }
+
+  uint8_t mpu_int_status;
+  uint8_t fifo_buffer[64];
+  Quaternion orientation;
+  VectorFloat gravity;
+  float tait_bryan[3];
+  VectorInt16 accel, world_accel;
+  float cos_heading, sin_heading;
+  float curr_forward_accel, curr_radial_accel;
+
+  // reset interrupt flag and get INT_STATUS byte
+  mpu_interrupt_ = false;
+  mpu_int_status = mpu_.getIntStatus();
+
+  // get current FIFO count
+  fifo_count_ = mpu_.getFIFOCount();
+
+  // check for FIFO overflow
+  if ((mpu_int_status & 0x10) || fifo_count_ == 1024) {
+    // reset so we can continue cleanly
+    mpu_.resetFIFO();
+    Serial2.println(F("FIFO overflow!"));
+
+  // check for DMP data ready interrupt
+  } else if (mpu_int_status & 0x01) {
+    // wait for correct available data length
+    while (fifo_count_ < packet_size_) {
+      fifo_count_ = mpu_.getFIFOCount();
+    }
+
+    // read a packet from FIFO
+    mpu_.getFIFOBytes(fifo_buffer, packet_size_);
+    
+    // track FIFO count here in case there is > 1 packet available
+    fifo_count_ -= packet_size_;
+
+    mpu_.dmpGetQuaternion(&orientation, fifo_buffer);
+    mpu_.dmpGetGravity(&gravity, &orientation);
+    mpu_.dmpGetYawPitchRoll(tait_bryan, &orientation, &gravity);
+
+    raw_heading_ = tait_bryan[0];
+
+    mpu_.dmpGetAccel(&accel, fifo_buffer);
+    mpu_.dmpGetLinearAccel(&accel, &accel, &gravity);
+    mpu_.dmpGetLinearAccelInWorld(&world_accel, &accel, &orientation);
+
+    cos_heading = cos(raw_heading_ * M_PI / 180.0);
+    sin_heading = sin(raw_heading_ * M_PI / 180.0);
+    curr_forward_accel = cos_heading * world_accel.x + sin_heading * world_accel.y;
+    curr_forward_accel = abs(curr_forward_accel) * 9.8 / 8192; // convert to m/s
+    curr_radial_accel = -sin_heading * world_accel.x + cos_heading * world_accel.y;
+    curr_radial_accel = abs(curr_radial_accel) * 9.8 / 8192; // convert to m/s
+
+    max_forward_accel_ = max(max_forward_accel_, curr_forward_accel);
+    max_radial_accel_ = max(max_radial_accel_, curr_radial_accel);
+  }
+}
+
+void Orientation::resetHeading() {
+  heading_offset_ = -raw_heading_;
+}
+
+float Orientation::getHeading() {
+  return raw_heading_ + heading_offset_;
+}
+
+void Orientation::resetMaxForwardAccel() {
+  max_forward_accel_ = 0;
+}
+
+void Orientation::resetMaxRadialAccel() {
+  max_radial_accel_ = 0;
+}
+
+float Orientation::getMaxForwardAccel() {
+  return max_forward_accel_;
+}
+
+float Orientation::getMaxRadialAccel() {
+  return max_radial_accel_;
+}
+
--- sensors_orientation.h
+++ sensors_orientation.h
@@ -1,12 +1,26 @@
 #ifndef MICROMOUSE_SENSORS_ORIENTATION_H_
 #define MICROMOUSE_SENSORS_ORIENTATION_H_
 
+#include "MPU6050_6Axis_MotionApps20.h"
+
 class Orientation {
   private:
     Orientation();
     static void interruptHandler();
 
-//    Orientation* _instance = NULL;
+    static volatile bool mpu_interrupt_;
+
+    Orientation* instance_ = NULL;
+    MPU6050 mpu_;
+    bool dmp_ready_ = false;
+    uint16_t packet_size_;
+    uint16_t fifo_count_ = 0;
+
+    float raw_heading_;
+    float heading_offset_ = 0;
+
+    float max_forward_accel_ = 0;
+    float max_radial_accel_ = 0;
   public:
     Orientation* getInstance();
 
