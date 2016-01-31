7cf5a28478421ac46b4a10159462c26a8365931d
Ryan Matthews <ryanmatthews@pitt.edu>
2016-01-31 12:58:44 -0500

    Clean up micromouse.ino
    Additionally, some code is moved to the loop function so that we can
    test the same code multiple times without power cycling the board.

--- micromouse.ino
+++ micromouse.ino
@@ -1,23 +1,28 @@
-#include "EncoderMod.h"
+#include <Arduino.h>
 #include "conf.h"
-#include "motors.h"
 #include "motion.h"
-#include "sensors_encoders.h"
 #include "RangeSensorContainer.h"
+#include "motors.h"
+#include "sensors_encoders.h"
+#include "EncoderMod.h"
 
 #define BAUD 9600
 
-int incomingByte = 0;
 void setup()
 {
-  analogWriteFrequency(MOTOR_AP_PIN, 46875);  // Set higher pwm frequency for smoother motor control
-  analogWriteFrequency(MOTOR_BP_PIN, 46875);  // Set higher pwm frequency for smoother motor control
-  analogWriteResolution(10);  // PWM resolution is 0-1023
+  // Set higher pwm frequency for smoother motor control.
+  analogWriteFrequency(MOTOR_AP_PIN, 46875);
+  analogWriteFrequency(MOTOR_BP_PIN, 46875);
 
-  // initialize all classes
-  Serial2.begin(BAUD);
-  
+  // PWM resolution is 0-1023.
+  analogWriteResolution(10);
+
   pinMode(BUTTON1_PIN, INPUT);
+}
+
+void loop()
+{
+  // Wait for button press.
   while (digitalRead(BUTTON1_PIN) == LOW);
   delay(1000);
   
@@ -26,9 +31,4 @@ void setup()
 
   motion_forward(600, 0);
   motion_hold(100);
-
-}
-
-void loop()
-{
 }
