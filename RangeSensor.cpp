23dd1a2c54962d09492749962f2648303406f5b7
Ryan Matthews <ryanmatthews@pitt.edu>
2016-02-29 23:16:16 -0500

    Adjust RangeSensor for new phototransistors

--- RangeSensor.cpp
+++ RangeSensor.cpp
@@ -5,6 +5,29 @@
 
 RangeSensor::RangeSensor(int temp_pin, int lowT, int highT) {
   pin_ = temp_pin;
+
+  switch (pin_) {
+    case RANGE1_PIN:
+      emitter_pin_ = EMITTER1_PIN;
+      constants_ = RANGE1_TRANSLATION;
+      break;
+
+    case RANGE2_PIN:
+      emitter_pin_ = EMITTER2_PIN;
+      constants_ = RANGE2_TRANSLATION;
+      break;
+
+    case RANGE3_PIN:
+      emitter_pin_ = EMITTER3_PIN;
+      constants_ = RANGE3_TRANSLATION;
+      break;
+
+    case RANGE5_PIN:
+      emitter_pin_ = EMITTER5_PIN;
+      constants_ = RANGE5_TRANSLATION;
+      break;
+  }
+
   low_threshold_ = lowT;
   high_threshold_ = highT;
   memset(raw_queue_, 0, sizeof(raw_queue_));
@@ -34,8 +57,25 @@ void RangeSensor::clearHistory() {
 
 //Update Raw Range Queue so ranges are accurate later when needed by higher level
 void RangeSensor::updateRange() {
+  float off_reading, on_reading;
+  float sensed_distance;
+
   raw_queue_sum_ -= raw_queue_[raw_queue_index_];
-  raw_queue_[raw_queue_index_] = (int) 20760 / (analogRead(pin_) - 11);
+
+  off_reading = analogRead(pin_);
+  digitalWrite(emitter_pin_, HIGH);
+
+  delayMicroseconds(45);
+
+  on_reading = analogRead(pin_);
+  digitalWrite(emitter_pin_, LOW);
+
+  delayMicroseconds(45);
+
+  sensed_distance = (constants_.a * pow((on_reading - off_reading
+          + constants_.b), constants_.c) + constants_.d) + constants_.e;
+
+  raw_queue_[raw_queue_index_] = (int) sensed_distance;
   raw_queue_sum_ += raw_queue_[raw_queue_index_];
   raw_queue_index_ = (raw_queue_index_ + 1) % RANGE_QUEUE_MAX_LENGTH;
 
