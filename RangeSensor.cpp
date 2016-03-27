380d9f9d15f0779fd83815519ccfa5d8fa022177
Aaron Miller <acmiller273@gmail.com>
2016-03-27 08:47:00 -0400

    Fixed range sensor readings of 0 at infinity

--- RangeSensor.cpp
+++ RangeSensor.cpp
@@ -51,11 +51,19 @@ void RangeSensor::updateRange() {
   last_reading_time_ = micros();
 
   if (sensed_distance < constants_.v0) {
-    sensed_distance = (constants_.a1 * pow((on_reading - off_reading
+    if (on_reading - off_reading - constants_.b1 < 0) {
+      sensed_distance = INFINITY;
+    } else {
+      sensed_distance = (constants_.a1 * pow((on_reading - off_reading
             - constants_.b1), constants_.c1) + constants_.d1) + constants_.e;
+    }
   } else {
-    sensed_distance = (constants_.a2 * pow((on_reading - off_reading
+    if (on_reading - off_reading - constants_.b2 < 0) {
+      sensed_distance = INFINITY;
+    } else {
+      sensed_distance = (constants_.a2 * pow((on_reading - off_reading
             - constants_.b2), constants_.c2) + constants_.d2) + constants_.e;
+    }
   }
 
   last_reading_ = sensed_distance;
