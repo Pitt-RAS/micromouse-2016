34590107899873f1c42d06c08c3b8928e1d064a2
Aaron Miller <acmiller273@gmail.com>
2016-03-29 01:42:59 -0400

    Fixed bugs in Logger

--- Logger.cpp
+++ Logger.cpp
@@ -22,27 +22,35 @@ void Logger::logMotionType(char value) {
   motion_type_log_.enqueue(value);
 }
 
-void Logger::logPrimaryPID(char value) {
+void Logger::logPrimaryPID(float value) {
   primary_pid_log_.enqueue(value);
 }
 
-void Logger::logSecondaryPID(char value) {
+void Logger::logSecondaryPID(float value) {
   secondary_pid_log_.enqueue(value);
 }
 
-void Logger::logTertiaryPID(char value) {
+void Logger::logTertiaryPID(float value) {
   tertiary_pid_log_.enqueue(value);
 }
 
 void Logger::nextCycle() {
   cycles_++;
 
+  if (forward_accel_log_.size() < cycles_) {
+    forward_accel_log_.enqueue(0);
+  }
+
+  if (radial_accel_log_.size() < cycles_) {
+    radial_accel_log_.enqueue(0);
+  }
+
   if (gyro_log_.size() < cycles_) {
     gyro_log_.enqueue(0);
   }
 
-  if (accel_log_.size() < cycles_) {
-    accel_log_.enqueue(0);
+  if (heading_log_.size() < cycles_) {
+    heading_log_.enqueue(0);
   }
 
   if (motion_type_log_.size() < cycles_) {
@@ -68,16 +76,21 @@ void Logger::dump() {
   while (cycles_ > 0) {
     Serial.print(motion_type_log_.dequeue());
     Serial.print("\t");
+    Serial.print(forward_accel_log_.dequeue());
+    Serial.print("\t");
+    Serial.print(radial_accel_log_.dequeue());
+    Serial.print("\t");
     Serial.print(gyro_log_.dequeue());
     Serial.print("\t");
-    Serial.print(accel_log_.dequeue());
+    Serial.print(heading_log_.dequeue());
     Serial.print("\t");
     Serial.print(primary_pid_log_.dequeue());
     Serial.print("\t");
     Serial.print(secondary_pid_log_.dequeue());
     Serial.print("\t");
-    Serial.print(primary_pid_log_.dequeue());
+    Serial.print(tertiary_pid_log_.dequeue());
     Serial.println();
+    cycles_--;
   }
 }
 
