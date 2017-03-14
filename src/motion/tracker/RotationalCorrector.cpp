0ac6f07557b980118fed9e638b70f42ae44e6e91
Ryan Matthews <ryanmatthews@pitt.edu>
2017-03-13 21:49:47 -0400

    RotationalCorrector range correction
    Including gyro calibration from range.

--- src/motion/tracker/RotationalCorrector.cpp
+++ src/motion/tracker/RotationalCorrector.cpp
@@ -1,7 +1,43 @@
 #include "../../device/Orientation.h"
+#include "../../device/RangeSensorContainer.h"
 #include "RotationalCorrector.h"
 
 namespace Motion {
+
+const LengthUnit GyroResetter::kTotalDistance = LengthUnit::fromCells(0.0);
+
+const LengthUnit GyroResetter::kDistancePerPoint = LengthUnit::fromAbstract(
+                                          kTotalDistance.abstract() / kPoints);
+
+const AngleUnit GyroResetter::kGyroLimit = AngleUnit::fromDegrees(0.0);
+const AngleUnit GyroResetter::kRangeLimit = AngleUnit::fromDegrees(0.0);
+
+GyroResetter::GyroResetter() :
+  gyro_(kGyroLimit.degrees()), range_(kRangeLimit.degrees())
+{}
+
+void GyroResetter::point(LinearRotationalPoint point)
+{
+  LengthUnit displacement = point.linear_point.displacement;
+
+  if (displacement.abstract() > kDistancePerPoint.abstract() * counter_++)
+    iteration(point);
+}
+
+void GyroResetter::iteration(LinearRotationalPoint point)
+{
+  AngleUnit gyro = AngleUnit::fromDegrees(-gOrientation->getHeading());
+  AngleUnit range = AngleUnit::fromDegrees(-RangeSensors.errorFromCenter());
+
+  gyro_.push(gyro.degrees());
+  range_.push(range.degrees());
+
+  if (gyro_.isStable() && range_.isStable()) {
+    gOrientation->incrementHeading(-gOrientation->getHeading());
+    gyro_.reset();
+    range_.reset();
+  }
+}
 
 RotationalCorrector::RotationalCorrector(PIDParameters gyro_pid_parameters,
                                           PIDParameters range_pid_parameters) :
@@ -11,11 +47,11 @@ RotationalCorrector::RotationalCorrector(PIDParameters gyro_pid_parameters,
 Matrix<double> RotationalCorrector::output(LinearRotationalPoint target)
 {
   gOrientation->update();
+  RangeSensors.updateReadings();
 
-  AngleUnit current = AngleUnit::fromDegrees(-gOrientation->getHeading());
-  AngleUnit setpoint = target.rotational_point.displacement;
+  updateResetter(target);
 
-  double response = gyro_pid_.response(current.degrees(), setpoint.degrees());
+  double response = gyroResponse(target) + rangeResponse(target);
 
   return Matrix<double>::splat(response).oppose();
 }
@@ -24,6 +60,27 @@ void RotationalCorrector::reset()
 {
   gyro_pid_.reset();
   range_pid_.reset();
+}
+
+void RotationalCorrector::updateResetter(LinearRotationalPoint point)
+{
+  gyro_resetter_.point(point);
+}
+
+double RotationalCorrector::gyroResponse(LinearRotationalPoint target)
+{
+  AngleUnit current = AngleUnit::fromDegrees(-gOrientation->getHeading());
+  AngleUnit setpoint = target.rotational_point.displacement;
+
+  return gyro_pid_.response(current.degrees(), setpoint.degrees());
+}
+
+double RotationalCorrector::rangeResponse(LinearRotationalPoint target)
+{
+  AngleUnit current = AngleUnit::fromDegrees(-RangeSensors.errorFromCenter());
+  AngleUnit setpoint = target.rotational_point.displacement;
+
+  return range_pid_.response(current.degrees(), setpoint.degrees());
 }
 
 }
--- src/motion/tracker/RotationalCorrector.h
+++ src/motion/tracker/RotationalCorrector.h
@@ -2,9 +2,34 @@
 #define ROTATIONAL_CORRECTOR_H
 
 #include "../PIDFunction.h"
+#include "../StabilityChecker.h"
 #include "StatefulOutput.h"
 
 namespace Motion {
+
+class GyroResetter
+{
+  public:
+    GyroResetter();
+
+    void point(LinearRotationalPoint point);
+
+  private:
+    static constexpr unsigned kPoints = 0;
+    static const LengthUnit kTotalDistance;
+
+    static const LengthUnit kDistancePerPoint;
+
+    static const AngleUnit kGyroLimit;
+    static const AngleUnit kRangeLimit;
+
+    void iteration(LinearRotationalPoint point);
+
+    StabilityChecker<kPoints> gyro_;
+    StabilityChecker<kPoints> range_;
+
+    int counter_ = -1;
+};
 
 class RotationalCorrector : public StatefulOutput
 {
@@ -17,8 +42,15 @@ class RotationalCorrector : public StatefulOutput
     virtual void reset();
 
   private:
+    void updateResetter(LinearRotationalPoint target);
+
+    double gyroResponse(LinearRotationalPoint target);
+    double rangeResponse(LinearRotationalPoint target);
+
     PIDFunction gyro_pid_;
     PIDFunction range_pid_;
+
+    GyroResetter gyro_resetter_;
 };
 
 }
